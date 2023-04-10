f06a1339998f1cbb47c5c28c8529fb76e9350c63
RodrigoTomeES <rotome@unirioja.es>
2023-04-10 17:21:54 +0200

    fix(removeLastDirectoryURL): now remove the / if is the last character

--- utils/url.ts
+++ utils/url.ts
@@ -2,4 +2,4 @@ export const getRealURL = (url: string | number) =>
   `${import.meta.env.BASE_URL.replace(/\/$/, '')}${url}`;
 
 export const removeLastDirectoryURL = (url: string) =>
-  url.substring(0, url.lastIndexOf('/'));
+  url.replace(/\/$/, '').substring(0, url.replace(/\/$/, '').lastIndexOf('/'));
