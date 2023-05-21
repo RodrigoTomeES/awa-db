c28f599b7cbc0fabcb0011be54490f8193588c8a
RodrigoTomeES <rotome@unirioja.es>
2023-05-21 18:29:22 +0200

    perf(routes): prevent redirects

--- utils/routes.ts
+++ utils/routes.ts
@@ -2,35 +2,29 @@ import { getRealURL } from './url';
 
 export const routes = {
   index: getRealURL('/'),
-  page: (page: number) => getRealURL(`/${page}`),
+  page: (page: number) => getRealURL(`/${page}/`),
   artifacts: {
-    index: getRealURL('/artifacts'),
     slug: 'artifacts',
-    page: (page: number) => getRealURL(`/artifacts/${page}`),
+    page: (page: number) => getRealURL(`/artifacts/${page}/`),
   },
   avatar: {
-    index: getRealURL('/avatars'),
     slug: 'avatars',
-    page: (page: number) => getRealURL(`/avatars/${page}`),
+    page: (page: number) => getRealURL(`/avatars/${page}/`),
   },
   badges: {
-    index: getRealURL('/badges-and-borders'),
     slug: 'badges-and-borders',
-    page: (page: number) => getRealURL(`/badges-and-borders/${page}`),
+    page: (page: number) => getRealURL(`/badges-and-borders/${page}/`),
   },
   gameVault: {
-    index: getRealURL('/game-vault'),
     slug: 'game-vault',
-    page: (page: number) => getRealURL(`/game-vault/${page}`),
+    page: (page: number) => getRealURL(`/game-vault/${page}/`),
   },
   dellRewards: {
-    index: getRealURL('/dell-rewards'),
     slug: 'dell-rewards',
-    page: (page: number) => getRealURL(`/dell-rewards/${page}`),
+    page: (page: number) => getRealURL(`/dell-rewards/${page}/`),
   },
   unknown: {
-    index: getRealURL('/unknown'),
     slug: 'unknown',
-    page: (page: number) => getRealURL(`/unknown/${page}`),
+    page: (page: number) => getRealURL(`/unknown/${page}/`),
   },
 };
