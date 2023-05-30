2f090cee1d8a83fb036b8dad080604724292a71f
RodrigoTomeES <rotome@unirioja.es>
2023-05-30 07:23:36 +0200

    feat: compress class with astro-rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,20 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import critters from 'astro-critters';
+import rename from 'astro-rename';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rodrigotomees.github.io',
   compressHTML: true,
+  output: 'static',
   integrations: [
     tailwind({
       config: {
         applyBaseStyles: false,
       },
     }),
+    rename(),
     critters(),
     compress({ img: false }),
     sitemap(),
